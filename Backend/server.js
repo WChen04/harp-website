9a4bbacbe4eef278823c5a96893074c4228a25cb
WChen04 <williamchen259@gmail.com>
2025-06-17 11:18:08 -0400

    nginx testing

--- Backend/server.js
+++ Backend/server.js
@@ -590,7 +590,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend is running" });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
 
